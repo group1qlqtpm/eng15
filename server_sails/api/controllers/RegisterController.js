ce3d8be287c51cdd75085df4fd2b98fc8a23df45
bong12345yt <thienbongyt@gmail.com>
2019-11-25 22:33:27 +0700

    login and register

---
+++ server_sails/api/controllers/RegisterController.js
@@ -0,0 +1,39 @@
+/**
+ * RegisterController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `RegisterController.username()`
+   */
+  username: async function (req, res) {
+    return res.json({
+      todo: 'username() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `RegisterController.email()`
+   */
+  email: async function (req, res) {
+    return res.json({
+      todo: 'email() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `RegisterController.passwd()`
+   */
+  passwd: async function (req, res) {
+    return res.json({
+      todo: 'passwd() is not implemented yet!'
+    });
+  }
+
+};
+

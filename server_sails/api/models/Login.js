835df1bf21f5eaacd66addb11a54b3e9852f923d
bong12345yt <thienbongyt@gmail.com>
2019-11-26 00:56:01 +0700

    fixed register and login generate!

--- server_sails/api/models/Login.js
+++ server_sails/api/models/Login.js
@@ -7,28 +7,65 @@
 
 module.exports = {
 
+  //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+  //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+  //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+  tableName: 'users',
+
+  // attributes: types, validations ans defaults values
   attributes: {
-
-    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
-    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
-    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
-    username: { type: 'string' },
-
-    email: { type: 'string' },
-
-    passwd: { type: 'string' }
-
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
+    id: {
+      type: 'integer',
+      autoIncrement: true,   
+    },
+    firstName: {
+      type: 'string',
+      required: true,
+    },
+    lastName: {
+      type: 'string',
+      required: true,
+    },
+    email: {
+      type: 'string',
+      unique: true,
+      required: true,
+    },
+    phone: {
+      type: 'string',
+      required: true
+    },
+    address: {
+      type: 'string',
+      required: true,
+    },
+    // createdData: {
+    //   type: 'string',
+    //   columnType: 'datetime'
+    // },
+    // updateDate: {
+    //   type: 'string',
+    //   columnType: 'datetime',
+    //   defaultsTo: function () {
+    //     return new Date();
+    //   }
+    // }
   },
+  // before execute function createsss
+  beforeCreate: function (values, cb) {
+    values.createdData = new Date();
+    cb();
+  }
+  //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+  //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+  //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+  //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+  //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
 
 };
 
